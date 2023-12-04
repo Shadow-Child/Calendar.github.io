5c11e1e158bfedede5d28ca89a9ab4892d3cd386
Shadow-Child <133650841+Shadow-Child@users.noreply.github.com>
2023-12-04 17:10:10 +0100

    Delete gcalendar directory

--- gcalendar/sw.js
+++
@@ -1,9 +0,0 @@
-// Installing Service Worker
-self.addEventListener('install', function(e) {
-  console.log('[Service Worker] Install');
-});
-
-// Fetching content using Service Worker
-self.addEventListener('fetch', function(e) {
-  //  console.log('[Service Worker] Caching new resource: '+e.request.url);
-});
